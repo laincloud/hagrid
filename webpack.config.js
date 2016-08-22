4fcaaaed4fdd2ece3d093c816b316185ff59417e
Jian Bai (Eric Pai) <ericpai94@hotmail.com>
2016-08-22 11:25:35 +0800

    Add lain.yaml and fix webpackage config

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         }]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx'],
+        fallback: "/usr/local/lib/node_modules"
     }
-
 };
